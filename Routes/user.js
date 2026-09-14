ed3422babe1a6d2860f1683e304f27cda818899e
Saksham Vilas Gawai <sakshamvilasgawai@gmail.com>
2026-09-14 13:07:37 +0530

    Add user profile view and update feature

--- Routes/user.js
+++ Routes/user.js
@@ -1,27 +1,54 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user.js");
+
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
-const {saveRedirectUrl}= require("../middleware.js");
+
+const {
+    saveRedirectUrl,
+    isLoggedIn
+} = require("../middleware.js");
 
 const userController = require("../controllers/user.js");
 
 router.route("/signup")
-.get(userController.renderSignupForm)
-.post(wrapAsync(userController.signup));
+    .get(userController.renderSignupForm)
+    .post(wrapAsync(userController.signup));
 
 router.route("/login")
-.get(userController.renderLoginForm)
-.post(
-    saveRedirectUrl,
-    passport.authenticate("local",{
-        failureRedirect:"/login",
-        failureFlash: true,
-    }),
-    userController.login
+    .get(userController.renderLoginForm)
+    .post(
+        saveRedirectUrl,
+        passport.authenticate("local", {
+            failureRedirect: "/login",
+            failureFlash: true,
+        }),
+        userController.login
+    );
+
+// PROFILE
+router.get(
+    "/profile",
+    isLoggedIn,
+    wrapAsync(userController.renderProfile)
 );
 
-router.post("/logout",userController.logout);
-    
+router.get(
+    "/profile/edit",
+    isLoggedIn,
+    wrapAsync(userController.renderEditProfile)
+);
+
+router.put(
+    "/profile",
+    isLoggedIn,
+    wrapAsync(userController.updateProfile)
+);
+
+// LOGOUT
+router.post(
+    "/logout",
+    userController.logout
+);
+
 module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,13 +1,32 @@
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose").default;
 
 const userSchema = new Schema({
-    email:{
+    email: {
         type: String,
-        required: true
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+
+    fullName: {
+        type: String,
+        trim: true,
+        default: ""
+    },
+
+    phone: {
+        type: String,
+        trim: true,
+        default: ""
     }
 });
 
-userSchema.plugin(passportLocalMongoose,{usernameField: "email"});
-module.exports = mongoose.model("User",userSchema); 
+userSchema.plugin(passportLocalMongoose, {
+    usernameField: "email"
+});
+
+module.exports = mongoose.model("User", userSchema);
+
